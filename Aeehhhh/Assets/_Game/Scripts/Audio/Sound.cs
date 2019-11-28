1c684d3d047d7ee1245e2949c0da41df765d5cb4
Norabacu <58297923+Norabacu@users.noreply.github.com>
2019-11-28 23:20:00 +0100

    Grass

---
+++ Aeehhhh/Assets/_Game/Scripts/Audio/Sound.cs
@@ -0,0 +1,22 @@
+﻿using Unity.Audio;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu(menuName = "Sound")]
+public class Sound : ScriptableObject
+{
+    public string name;
+    
+    public AudioClip clip;
+    
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(.1f, 3f)]
+    public float pitch;
+
+    public bool loop;
+    public bool playOnAwake;
+
+    [HideInInspector]
+    public AudioSource source;
+}
