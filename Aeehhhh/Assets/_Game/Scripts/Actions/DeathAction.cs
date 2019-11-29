847f0b404e30005fbdc36fd021e8f740c93731bc
M-e-e <44639352+M-e-e@users.noreply.github.com>
2019-11-29 01:18:17 +0100

    Prefab Overload

---
+++ Aeehhhh/Assets/_Game/Scripts/Actions/DeathAction.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityAtoms;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Aeehhhh/Actions/Death")]
+public class DeathAction : GameObjectAction
+{
+    public override void Do(GameObject t1)
+    {
+        Destroy(t1);
+    }
+}
