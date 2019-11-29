ab2c703f08a235fca40c465cecd1fdcf131710ee
M-e-e <44639352+M-e-e@users.noreply.github.com>
2019-11-29 14:17:11 +0100

    -Player attack cooldown [FIXED] -Character Prefab implemented

---
+++ Aeehhhh/Assets/_Game/Scripts/Character/Player/AttackInputPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityAtoms;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class AttackInputPlayer : MonoBehaviour
+{
+    [SerializeField] private AttackAction _attackAction;
+
+    [SerializeField] private FloatReference attackCooldown;
+    
+    [SerializeField] private VoidEvent punchEvent;
+
+    private void Start()
+    {
+        Observable.EveryUpdate().Where(_ => Input.GetMouseButtonDown(0))
+            .ThrottleFirst(TimeSpan.FromSeconds(attackCooldown.Value))
+            .Subscribe(_ =>
+            {
+                var pos = Input.mousePosition;
+                pos.z = 10;
+                pos = Camera.main.ScreenToWorldPoint(pos);
+
+                _attackAction.Do(gameObject, pos - transform.position);
+                punchEvent.Raise();
+            });
+    }
+}
